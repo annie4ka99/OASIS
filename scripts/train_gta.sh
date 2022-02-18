a0d056d33f160f614cb47088b2dfef166d1c9da0
Anna Potanina <potanya.99@gmail.com>
2022-02-18 17:04:19 +0500

    Create train_gta.sh

---
+++ scripts/train_gta.sh
@@ -0,0 +1,1 @@
+python train.py --name oasis_gta --dataset_mode gta --dataroot data/gta --batch_size 2 --freq_save_ckpt 10000 --freq_save_latest 1000 --freq_save_loss 1000 --num_epochs 50 --continue_train
