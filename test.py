85b7ece64c83cd231acbe503d5736f36d1c5885f
annie4ka99 <47689680+annie4ka99@users.noreply.github.com>
2022-03-27 22:38:25 +0300

    Update test.py

--- test.py
+++ test.py
@@ -20,7 +20,11 @@ model = models.put_on_multi_gpus(model, opt)
 model.eval()
 
 #--- iterate over validation set ---#
+start_time = time.time()
+data_size = len(dataloader_val.dataset)
 for i, data_i in enumerate(dataloader_val):
     _, label = models.preprocess_input(opt, data_i)
     generated = model(None, label, "generate", None)
     image_saver(label, generated, data_i["name"])
+avg = (time.time() - start_time) / data_size
+print(print('avg time for 1 image:%.3f' % avg))
